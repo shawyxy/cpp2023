13161c3cd4f258e24875305a8dcdf301262b0795
shawyxy <9inghuan@gmail.com>
2023-02-26 20:18:14 +0800

    完美转发

--- 23_2_26_test/main.cpp
+++ 23_2_26_test/main.cpp
@@ -35,111 +35,194 @@
 //
 //    return 0;
 //}
+//#include <iostream>
+//using namespace std;
+//
+//void Func(int& x)
+//{
+//    cout << "左值引用" << endl;
+//}
+//void Func(const int& x)
+//{
+//    cout << "const 左值引用" << endl;
+//}
+//void Func(int&& x)
+//{
+//    cout << "右值引用" << endl;
+//}
+//void Func(const int&& x)
+//{
+//    cout << "const 右值引用" << endl;
+//}
+//
+//template<class T>
+//void PerfectForward(T&& t)
+//{
+//    // 使用std::forward进行类型转换
+//    Func(std::forward<T>(t));
+//}
+//int main()
+//{
+//    int a = 1;
+//    PerfectForward(a);          // 左值
+//    PerfectForward(move(a)); // 右值
+//
+//    const int b = 2;
+//    PerfectForward(b);          // const 左值
+//    PerfectForward(move(b)); // const 右值
+//
+//    return 0;
+//}
 #include <iostream>
-#include <memory>
 using namespace std;
-namespace xy {
-
-
-    template<typename T>
-    class vector {
-    private:
-        T *data; // 指向动态数组的指针
-        size_t size; // 已使用的空间
-        size_t capacity; // 总容量
+#include <string>
+#include <cstddef>
+namespace xy
+{
+    class string
+    {
     public:
-        vector() : data(nullptr), size(0), capacity(0) {} // 默认构造函数
-        ~vector() { delete[] data; } // 析构函数
+        // 构造函数
+        string(const char* str = "")
+        {
+            _size = strlen(str);
+            _capacity = _size;
+            _str = new char[_capacity + 1];
+            strcpy(_str, str);
+        }
+        void swap(string& s)
+        {
+            std::swap(_str, s._str);
+            std::swap(_size, s._size);
+            std::swap(_capacity, s._capacity);
+        }
+        // *拷贝构造函数
+        string(const string& s)
+                :_str(nullptr)
+                , _size(0)
+                , _capacity(0)
+        {
+            cout << "string(const string& s) -- 深拷贝" << endl;
 
-        void push_back(const T &x) { // 在末尾添加一个元素（拷贝）
-            if (size == capacity) { // 如果空间不足，需要重新分配
-                resize(2 * capacity + 1); // 分配两倍多一点的空间
+            string tmp(s._str);
+            swap(tmp);
+        }
+        // *赋值运算符重载
+        string& operator=(const string& s)
+        {
+            cout << "string& operator=(const string& s) -- 深拷贝" << endl;
+
+            string tmp(s);
+            swap(tmp);
+            return *this;
+
+        }
+        // 移动赋值
+        string& operator=(string&& s)
+        {
+            cout << "string& operator=(string&& s) -- 移动赋值" << endl;
+
+            string tmp(s);
+            swap(tmp);
+            return *this;
+
+        }
+        void push_back(char ch)
+        {
+            if (_size == _capacity)
+            {
+                reserve(_capacity == 0 ? 4 : _capacity * 2);
             }
-            data[size++] = x; // 拷贝元素到末尾，并更新size
+            _str[_size] = ch;
+            _str[_size + 1] = '\0';
+            _size++;
+        }
+        // +=运算符重载
+        string& operator+=(char ch)
+        {
+            push_back(ch);
+            return *this;
+        }
+        void reserve(size_t n)
+        {
+            if (n > _capacity)
+            {
+                char* tmp = new char[n + 1];
+                strncpy(tmp, _str, _size + 1);
+                delete[] _str;
+                _str = tmp;
+                _capacity = n;
+            }
+        }
+        // 其他接口...
+    private:
+        char* _str;
+        size_t _size;
+        size_t _capacity;
+    };
+}
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+namespace xy
+{
+    template<typename T>
+    class vector
+    {
+    public:
+        vector() // 默认构造函数
+        : data(nullptr), size(0), capacity()
+        {}
+        ~vector() // 析构函数
+        {
+            delete[] data;
+        }
+        void init()
+        {
+            data = (T*) malloc(sizeof(T) * 100);
+        }
+        void push_back(const T &x) // 在末尾添加一个元素（拷贝）
+        {
+            insert(size, x);
         }
 
-        void push_back(T &&x) { // 在末尾添加一个元素（移动）
-            if (size == capacity) {
-                resize(2 * capacity + 1);
-            }
-            data[size++] = std::move(x); // 移动元素到末尾，并更新size
+        void push_back(T &&x) // 在末尾添加一个元素（移动）
+        {
+            insert(size, std::forward<T>(x));
         }
-
-        template<typename... Args>
-        void emplace_back(Args &&... args) { // 在末尾添加一个元素（完美转发）
-            if (size == capacity) {
-                resize(2 * capacity + 1);
-            }
-            new(data + size++) T(std::forward<Args>(args)...); // 在原地构造元素，并更新size
-        }
-
-        void insert(size_t pos, const T &x) { // 在指定位置插入一个元素（拷贝）
-            if (pos > size) return; // 如果位置无效，直接返回
-            if (size == capacity) {
-                resize(2 * capacity + 1);
-            }
-            for (size_t i = size; i > pos; i--) {
+        void insert(size_t pos, const T &x) // 在指定位置插入一个元素（拷贝）
+        {
+            for (size_t i = size; i > pos; i--)
+            {
                 data[i] = data[i - 1]; // 将pos之后的元素后移一位
             }
             data[pos] = x; // 拷贝元素到pos位置，并更新size
             size++;
         }
 
-        void insert(size_t pos, T &&x) { // 在指定位置插入一个元素（移动）
-            if (pos > size) return;
-            if (size == capacity) {
-                resize(2 * capacity + 1);
+        void insert(size_t pos, T &&x) // 在指定位置插入一个元素（移动）
+        {
+            for (size_t i = size; i > pos; i--)
+            {
+                data[i] = std::forward<T>(data[i - 1]);
             }
-            for (size_t i = size; i > pos; i--) {
-                data[i] = std::move(data[i - 1]);
-            }
-            data[pos] = std::move(x);
+            data[pos] = std::forward<T>(x);
             size++;
         }
-
-        template<typename... Args>
-        void emplace(size_t pos, Args &&... args) { // 在指定位置插入一个元素（完美转发）
-            if (pos > size) return;
-            if (size == capacity) {
-                resize(2 * capacity + 1);
-            }
-            for (size_t i = size; i > pos; i--) {
-                new(data + i)T(std::move(data[i - 1]));
-                data[i - 1].~T();
-            }
-            new(data + pos)T(std::forward<Args>(args)...);
-            size++;
-        }
-
-        void print() const {
-            for (size_t i = 0; i < size; i++) {
-                cout << data[i] << " ";
-            }
-            cout << endl;
-        }
-
     private:
-        void resize(size_t new_capacity) {
-            if (new_capacity < size)return;
-            T *new_data = new T[new_capacity];
-            for (size_t i = 0; i < size; i++) {
-                new_data[i] = std::move(data[i]);
-            }
-            delete[]data;
-            data = new_data;
-            capacity = new_capacity;
-        }
+        T *data; // 指向动态数组的指针
+        size_t size; // 已使用的空间
+        size_t capacity; // 总容量
     };
 }
-    int main() {
-        xy::vector<int> v;
-        v.push_back(10);
-        v.print();
+int main()
+{
+    xy::vector<xy::string> v;
+    v.init();
 
-        v.emplace_back(20);
-        v.print();
-
-        v.insert(0,30);
-        v.print();
-
+    v.push_back("1"); // 右值引用
+    cout << "右值引用↑----------------左值引用↓" << endl;
+    xy::string s1("3"); // 左值引用
+    v.push_back(s1);
+    return 0;
 }
