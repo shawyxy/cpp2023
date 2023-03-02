acdecbc6423a8dca1379d3a499de2c065779da87
shawyxy <9inghuan@gmail.com>
2023-03-02 18:06:53 +0800

    bind包装器

---
+++ 23_3_2_test/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+//int add(int a, int b)
+//{
+//    return a + b;
+//}
+//int subtract(int a, int b)
+//{
+//    return a - b;
+//}
+//int main()
+//{
+//    function<int(int, int)> f1 = bind(add, placeholders::_1, placeholders::_2);
+//    cout << f1(1, 2) << endl;
+//
+//    function<int(int)> f2 = bind(add, placeholders::_1, 2);
+//    cout << f2(1) << endl;
+//
+//    function<int(int)> f3= bind(add, 1, placeholders::_1);
+//    cout << f3(2) << endl;
+//
+//    function<int(int, int)> f4= bind(subtract, placeholders::_2, placeholders::_1);
+//    cout << f4(2, 1) << endl;
+//    return 0;
+//}
