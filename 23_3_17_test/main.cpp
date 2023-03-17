742c9f6c3b0ed37f672c82a83892fa9d299fa82a
shawyxy <9inghuan@gmail.com>
2023-03-17 18:26:54 +0800

    字符串拼接+fib longlong

---
+++ 23_3_17_test/main.cpp
@@ -0,0 +1,54 @@
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//    int n = 0;
+//    while(cin >> n)
+//    {
+//        int a = 1, b = 1;
+//        for(int i = 2; i <= n; i++)
+//        {
+//            int tmp = a + b;
+//            a = b;
+//            b = tmp;
+//        }
+//        cout << b << endl;
+//    }
+//    return 0;
+//}
+
+//int main()
+//{
+//    int n = 0;
+//    while (cin >> n)
+//    {
+//        cin.ignore();
+//        string ans = "";
+//        for (int i = 0; i < n; i++)
+//        {
+//            string name;
+//            getline(cin, name);
+//            if (name.find(',') != string::npos)
+//            {
+//                ans += "\"" + name + "\"";
+//            }
+//            else
+//            {
+//                ans += name;
+//            }
+//
+//            if (i == n - 1)
+//            {
+//                break;
+//            }
+//            ans += ", ";
+//        }
+//        cout << ans << endl;
+//    }
+//
+//    return 0;
+//}
+// write your code here cpp
